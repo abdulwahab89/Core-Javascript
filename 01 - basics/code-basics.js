b4323914d58efb5f8516c44a3e462bd50d70c7eb
Abdul Wahab Baig <110450008+abdulwahab89@users.noreply.github.com>
2024-12-15 16:38:20 +0500

    functions

---
+++ 01 - basics/code-basics.js
@@ -0,0 +1,13 @@
+// Functions
+console.log(getMyName("Wahab"));
+
+function getMyName(name){
+    return name;
+}
+add(4,5)
+function add(num1,num2){
+    const number=num1+num2;
+    console.log(`Result is ${number}`);
+    
+}
+// so JS takes all functions when reading a file and then it doesnt matters if you initialize a function later in your code.
---
+++ 01 - basics/date_time.js
@@ -0,0 +1,10 @@
+let myDate=new Date();
+console.log(`Current date:${myDate.toString()} `)
+console.log(`Current hours :${myDate.getHours()} `)
+console.log(`Current Time:${myDate.toLocaleTimeString()}`)
+let currentData=Date.now()
+const options={
+    timezone:'America/New_York',
+    timeZoneName:'short',
+}
+console.log(new Intil.DateTimeFormat('en-US',options).format(date))
