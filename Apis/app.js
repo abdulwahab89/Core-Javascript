d091e9983cba62235112ce6d84a821dc15262eef
Abdul Wahab Baig <110450008+abdulwahab89@users.noreply.github.com>
2024-12-27 15:51:10 +0500

    Finish

--- Apis/app.js
+++ Apis/app.js
@@ -1,35 +1,62 @@
-// API --? Application Programming Interface
+// // API --? Application Programming Interface
 
 
- const URL="https://jsonplaceholder.typicode.com/todos";
-// fetch api -- uses request and response objects
+//  const URL="https://jsonplaceholder.typicode.com/todos";
+// // fetch api -- uses request and response objects
 
-const todoPara=document.querySelector("#todo");
-const btn=document.querySelector("#btn")
-// const getFacts= async()=>{ 
-//     let response=await fetch(URL);
-// let data= await response.json();
+// const todoPara=document.querySelector("#todo");
+// const btn=document.querySelector("#btn")
+// // const getFacts= async()=>{ 
+// //     let response=await fetch(URL);
+// // let data= await response.json();
 
-// console.log(data[0].title);
-// todoPara.innerText=data[1].title;
+// // console.log(data[0].title);
+// // todoPara.innerText=data[1].title;
+// // }
+
+
+// function getFacts(){
+//     fetch(URL).then((response)=>{
+//         return response.json();
+//     }).then((data)=>{
+//         console.log(data); 
+//         todoPara.innerText=data[1].title;
+//     })
 // }
 
-
-function getFacts(){
-    fetch(URL).then((response)=>{
-        return response.json();
-    }).then((data)=>{
-        console.log(data); 
-        todoPara.innerText=data[1].title;
-    })
-}
-
-btn.addEventListener("click",getFacts)
+// btn.addEventListener("click",getFacts)
 
 
-// AJAX is asynchronous JS and XML
+// // AJAX is asynchronous JS and XML
 
-// JSON is Javascript Object Notation --> AJAJ, asynchronous JS and JSON
+// // JSON is Javascript Object Notation --> AJAJ, asynchronous JS and JSON
 
 
 
+
+// // REQUEST AND RESPONSE.
+
+// //HTTP verbs
+// //Response status code : declares the health status of the request
+
+// // 200 --> good 
+// // 400 --> bad request 
+// //404 ---> client error messsge , page not found
+// //500 > server error responses 
+
+// //headers:: Extra information 
+// // content type , status code etc
+
+ const BASE_URL="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json"
+
+ const dropdowns=document.querySelector(".dropdown select")
+
+ for(let select of dropdowns){
+    for(currCode in countryList){
+       let newOptions=document.createElement("option")
+        newOptions.innerText=currCode;
+        newOptions.value=currCode;
+    select.append(newOptions);
+    }
+
+ }
